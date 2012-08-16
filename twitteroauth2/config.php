0f16af45afadb17244eecf23571a4c3381d7e66f
root <root@dti-vps-srv120.(none)>
2012-08-16 17:03:31 +0900

    rename twitteroauth

---
+++ twitteroauth2/config.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', 'OGTWud5yfmmJvdLmGAjk0A');
+define('CONSUMER_SECRET', 'MrVGsjYLHonCVZgKVOubYKG1dTI7HD7fxATjJRlS1HM');
+define('OAUTH_CALLBACK', 'http://27.120.111.32/twitter_api/smarty/timeline.php');
+//define('OAUTH_CALLBACK', 'http://example.com/twitteroauth/callback.php');
